bc978a4c248aad0739f0af2f0c2b68d4df138a1c
PINK <henghengrahman@gmail.com>
2026-09-20 17:57:17 +0700

    Delete scripts directory

--- scripts/generate-secrets.js
+++
@@ -1,3 +0,0 @@
-import crypto from 'node:crypto';
-console.log('SESSION_SECRET='+crypto.randomBytes(48).toString('base64url'));
-console.log('SESSION_VAULT_KEY='+crypto.randomBytes(48).toString('base64url'));
