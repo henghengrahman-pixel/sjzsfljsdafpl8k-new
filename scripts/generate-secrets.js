4d89c02ed0118661bbf90c64b00c0a9800f28482
PINK <henghengrahman@gmail.com>
2026-09-20 14:51:31 +0700

    Update generate-secrets.js

--- scripts/generate-secrets.js
+++ scripts/generate-secrets.js
@@ -1,5 +1,3 @@
 import crypto from 'node:crypto';
-import { authenticator } from 'otplib';
 console.log('SESSION_SECRET='+crypto.randomBytes(48).toString('base64url'));
 console.log('SESSION_VAULT_KEY='+crypto.randomBytes(48).toString('base64url'));
-console.log('MASTER_TOTP_SECRET='+authenticator.generateSecret());
