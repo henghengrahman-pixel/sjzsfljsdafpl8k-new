c199f7b19b756100d024562f0383cb3a3a324889
PINK <henghengrahman@gmail.com>
2026-09-20 18:22:49 +0700

    Delete test directory

--- test/smoke.test.js
+++
@@ -1,7 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { encryptText,decryptText } from '../src/security.js';
-import { parseDateRange } from '../src/report.js';
-test('vault encrypt/decrypt',()=>{const s='x'.repeat(32);const c=encryptText('PHPSESSID=abc',s);assert.notEqual(c,'PHPSESSID=abc');assert.equal(decryptText(c,s),'PHPSESSID=abc')});
-test('date range',()=>assert.deepEqual(parseDateRange('2026-09-15','2026-09-16'),{from:'2026-09-15',to:'2026-09-16'}));
-test('invalid date rejected',()=>assert.throws(()=>parseDateRange('15-09-2026','2026-09-16')));
